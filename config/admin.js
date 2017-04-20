95dbf9aebf4425554ead9b0dc04e6b473db65dd6
softdream815 <uyg0815@gmail.com>
2017-04-20 16:54:36 +0300

    Refactoring configuration

--- config/admin.js
+++ config/admin.js
@@ -1,7 +1,6 @@
 // config used by dashboard client side only
-let domain = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
 module.exports = {
   // dashboard UI language
   language: 'en',
-  apiBaseUrl: `${domain}/api/v1`
+  apiBaseUrl: '/api/v1'
 }
