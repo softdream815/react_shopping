74db35cef919616f03edfe36d6ffac62559dd92e
softdream815 <uyg0815@gmail.com>
2018-01-25 22:55:29 +0200

    Fixed cookies on different domains with Access-Control-Allow-Origin.

--- src/api/server/lib/mongo.js
+++ src/api/server/lib/mongo.js
@@ -1,38 +1,18 @@
 const settings = require('./settings');
 const winston = require('winston');
-const MongoClient = require('mongodb').MongoClient;
-
+const mongo = require('mongodb').MongoClient;
 const mongodbConnection = settings.mongodbServerUrl;
-const lastslashindex = mongodbConnection.lastIndexOf('/');
-const dbName = mongodbConnection.substring(lastslashindex  + 1);
 
-const RECONNECT_INTERVAL = 1000;
 const CONNECT_OPTIONS = {
   reconnectTries: 3600,
-  reconnectInterval: RECONNECT_INTERVAL
+  reconnectInterval: 1000
 }
 
-const onClose = () => {
-  winston.info('MongoDB connection was closed');
-}
-
-const onReconnect = () => {
-  winston.info('MongoDB reconnected');
-}
-
-const connectWithRetry = () => {
-  MongoClient.connect(mongodbConnection, CONNECT_OPTIONS, (err, client) => {
-    if(err){
-      winston.error('MongoDB connection was failed:', err.message);
-      setTimeout(connectWithRetry, RECONNECT_INTERVAL);
-    } else {
-      const db = client.db(dbName);
-      db.on('close', onClose);
-      db.on('reconnect', onReconnect);
-      module.exports.db = db;
-      winston.info('MongoDB connected successfully');
-    }
-  });
-};
-
-connectWithRetry();
+mongo.connect(mongodbConnection, CONNECT_OPTIONS, (err, client) => {
+  if(err){
+    winston.error('Failed connecting to MongoDB', err.message);
+  } else {
+    module.exports.db = client.db('shop');
+    winston.info('Successfully connected to MongoDB')
+  }
+});
